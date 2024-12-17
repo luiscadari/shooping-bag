40df1c7885b27c0783779b123df2776ff9ac24e0
luiscadari <luiscadari01@gmail.com>
2024-12-17 16:48:58 -0300

    :sparkles: feat: LocalStorage Bag
    - Carrinho do usuário fica salvo no localStorage do navegador, permitindo que o cliente possa acessar o carrinho de diferentes abas, ou que ele possa recarregar a página e não perder seu carrinho

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import axios from "axios";
 export default {
     created() {
         this.$store.dispatch("fetchProducts");
+        this.$store.dispatch("loadBag");
     },
     computed: mapState(["productsInBag"]),
 };
--- src/store/index.js
+++ src/store/index.js
@@ -12,12 +12,24 @@ export default createStore({
       console.log(products);
       state.products = products;
     },
+    loadBag(state, products) {
+      var bag = JSON.parse(localStorage.getItem("productsInBag"));
+      state.productsInBag = [...bag];
+    },
     addToBag(state, product) {
       state.productsInBag.push(product);
+      localStorage.setItem(
+        "productsInBag",
+        JSON.stringify(state.productsInBag),
+      );
     },
     removeFromBag(state, productId) {
       var updatedBag = state.productsInBag.filter((p) => p.id !== productId);
       state.productsInBag = updatedBag;
+      localStorage.setItem(
+        "productsInBag",
+        JSON.stringify(state.productsInBag),
+      );
     },
   },
   actions: {
@@ -28,6 +40,11 @@ export default createStore({
     },
     addToBag({ commit }, product) {
       commit("addToBag", product);
+    },
+    loadBag({ commit }) {
+      if (localStorage.getItem("productsInBag")) {
+        commit("loadBag", JSON.parse(localStorage.getItem("productsInBag")));
+      }
     },
     removeFromBag({ commit }, productId) {
       if (confirm("Are you sure you want to remove the item from bag?")) {
