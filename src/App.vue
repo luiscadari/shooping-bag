87d2fcf859ee0b1747ce91869a044a8d8eda17cf
luiscadari <luiscadari01@gmail.com>
2024-12-17 09:14:52 -0300

    :sparkles: feat: Vuex config
    - Coletando dados de uma api usando Vuex;

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,7 @@
 import axios from "axios";
 export default {
     created() {
-        axios.get("https://fakestoreapi.com/products").then((response) => {
-            console.log(response.data);
-        });
+        this.$store.dispatch("fetchProducts");
     },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,23 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
   state: {
-    
+    //dados
+    products: [],
   },
   mutations: {
-    
+    fetchProducts(state, products) {
+      console.log(products);
+      state.products = products;
+    },
   },
   actions: {
-
+    fetchProducts({ commit }) {
+      axios.get("https://fakestoreapi.com/products").then((response) => {
+        commit("fetchProducts", response.data);
+      });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
